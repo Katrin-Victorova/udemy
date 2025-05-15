8e1c36671469f4bd74ec8c3048f4a965b1e767ef
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-15 10:18:50 +0300

    JS_step_2

--- JS_step_1/script.js
+++ JS_step_1/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -7,15 +7,15 @@ const personalMovieDB = {
 	genres: [],
 	count: numberOfFilms,
 	privat: false,
-}
+};
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
 	b = prompt('На сколько оцените его?'),
 	c = prompt('Один из последних просмотренных фильмов?', ''),
-	d = prompt('На сколько оцените его?')
+	d = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 // через movies.c не все данные запишуться
 
-console.log(personalMovieDB)
+console.log(personalMovieDB);
